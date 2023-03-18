e634c7c00d7ad3f7bfa6c323ac57effe24082926
Alabi Olalekan Emmanuel <alabilekanemmanuel@gmail.com>
2023-03-19 00:18:48 +0100

    code maintenance

--- src/components/pages/Checkout.jsx
+++ src/components/pages/Checkout.jsx
@@ -8,7 +8,7 @@ import backArrow from "../../assets/back-arrow.svg";
 
 const Checkout = () => {
   const { subTotalPrice } = useContext(ProductDataContext);
-  const shippingPrice = 3.25;
+  const shippingPrice = subTotalPrice > 100 ? 3.25 : 0;
   const totalPrice = subTotalPrice + shippingPrice;
   const navigate = useNavigate();
   const user = useAuth();
@@ -38,12 +38,25 @@ const Checkout = () => {
     }
   }, []);
 
+  // Handle Back Button
    const handleBack = () => {
      navigate(-1);
    };
 
+  //  Get date for delivery in Month, Day, Year format
+  const deliveryDate = new Date();
+  deliveryDate.setDate(deliveryDate.getDate() + 7);
+  const deliveryDateFormatted = deliveryDate.toLocaleDateString("en-US", {
+    month: "long",
+    day: "numeric",
+    year: "numeric",
+  });
+
+
+
+
   return (
-    <div className="bg-gray-100 py-10 mt-6">
+    <div className="py-10 mt-6 bg-[#FDFBFB] ">
       <div className="w-[30px] h-[30px] ml-4 flex justify-center items-center mb-6 bg-[rgba(225,225,225,0.2)] rounded-[100%] cursor-pointer hover:scale-110">
         <img
           onClick={handleBack}
@@ -52,12 +65,12 @@ const Checkout = () => {
           className="w-[15px] h-[15px]"
         />
       </div>
-      <div className="max-w-6xl mx-auto px-4">
-        <div className="bg-white shadow-lg rounded-lg p-6">
+      <div className="max-w-6xl mx-auto px-4 ">
+        <div className="bg-white border shadow-lg rounded-lg p-6">
           <h2 className="text-xl font-semibold mb-4">Checkout</h2>
           <div className="flex flex-col md:flex-row md:justify-between">
             <div className="w-full md:w-1/2 md:pr-4 mb-4 md:mb-0">
-              <h3 className="text-lg font-semibold mb-2">Shipping address</h3>
+              <h3 className="text-lg font-semibold mb-2">Shipping details</h3>
               <p className="text-gray-700 mb-2">
                 {user?.displayName || email}
                 <br />
@@ -72,15 +85,9 @@ const Checkout = () => {
               <div className="bg-gray-200 p-4 rounded-lg mb-2">
                 <p className="text-gray-700 mb-2">Standard Shipping</p>
                 <p className="text-gray-700">
-                  Estimated delivery date: March 15, 2023
+                  Estimated delivery date: {deliveryDateFormatted}
                 </p>
               </div>
-              <button
-                to="/profile"
-                className="text-blue-500 hover:text-blue-600"
-              >
-                Change
-              </button>
             </div>
           </div>
           <div className="flex flex-col md:flex-row md:justify-between">
